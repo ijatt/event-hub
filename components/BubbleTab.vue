e030777f0372bba753a0a118f793f72286cbd6db
Ijatt <mshahrizat05@gmail.com>
2024-12-25 18:07:48 +0800

    mobile responsive

--- components/BubbleTab.vue
+++ components/BubbleTab.vue
@@ -3,7 +3,7 @@
         <div class="rounded-full p-6 border border-slate-300 flex items-center w-max cursor-pointer hover:shadow-md">
           <Icon :name="icon" size="48" class="text-slate-500" />
         </div>
-        <p class="text-sm text-slate-600 text-wrap text-center mt-1">{{ text }}</p>
+        <p class="text-sm text-slate-600 text-nowrap text-center mt-1">{{ text }}</p>
       </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="py-10">
     <TheCarousell />
-    <div class="mt-10 flex w-full justify-between items-center px-28">
+    <div class="mt-10 flex w-5/6 lg:w-full space-x-4 max-w-screen-lg mx-auto justify-between items-center lg:px-28 overflow-x-auto">
       <BubbleTab icon="guidance:calendar" text="Events" />
       <BubbleTab icon="material-symbols-light:fastfood-outline-sharp" text="Food and Drinks" />
       <BubbleTab icon="ph:disco-ball-thin" text="Nightlife" />
       <BubbleTab icon="arcticons:games-2" text="Hobbies" />
       <BubbleTab icon="material-symbols-light:mic-outline" text="Music" />
     </div>
-    <div class="mt-10 border-t border-slate-300 py-4">
+    <div class="mx-auto w-5/6 lg:w-full max-w-screen-lg mt-10 border-t border-slate-300 py-4">
       <h1 class="text-2xl font-semibold tracking-wide text-slate-700">
         Events in Shah Alam
       </h1>
@@ -25,5 +25,3 @@
 </template>
 
 <script lang="ts" setup></script>
-
-<style></style>
