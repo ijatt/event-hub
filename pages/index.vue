73aaa24936f547f085027e35460ae5d9eba18c56
Ijatt <mshahrizat05@gmail.com>
2025-01-19 21:43:53 +0800

    upload image

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,10 @@
   <div class="py-10">
   <TheCarousell />
     <div class="mt-10 flex w-5/6 lg:w-full space-x-4 max-w-screen-lg mx-auto justify-between items-center lg:px-28 overflow-x-auto">
-      <BubbleTab icon="guidance:calendar" text="Events" :isActive="false" @click="activeTag = 'events'"/>
-      <BubbleTab icon="material-symbols-light:fastfood-outline-sharp" text="Food and Drinks" :isActive="false" @click="activeTag = 'food'"/>
-      <BubbleTab icon="ph:disco-ball-thin" text="Nightlife" :isActive="false"  @click="activeTag = 'nightlife'"/>
-      <BubbleTab icon="arcticons:games-2" text="Hobbies" :isActive="false"  @click="activeTag = 'hobbies'"/>
+      <BubbleTab icon="guidance:calendar" text="Events" :isActive="false" @click="(activeTag = 'events', navigateTo('#events'))"/>
+      <BubbleTab icon="material-symbols-light:fastfood-outline-sharp" text="Food and Drinks" :isActive="false" @click="(activeTag = 'food', navigateTo('#food'))"/>
+      <BubbleTab icon="ph:disco-ball-thin" text="Nightlife" :isActive="false"  @click="(activeTag = 'nightlife', navigateTo('#nightlife'))"/>
+      <BubbleTab icon="arcticons:games-2" text="Hobbies" :isActive="false"  @click="(activeTag = 'hobbies', navigateTo('#hobbies'))"/>
       <BubbleTab icon="material-symbols-light:mic-outline" text="Music" :isActive="false"  @click="(activeTag = 'music', navigateTo('#music'))"/>
     </div>
     <div class="mx-auto w-5/6 lg:w-full max-w-screen-lg mt-10 border-t dark:border-slate-700 border-slate-300 py-4">
@@ -30,7 +30,7 @@
         <template v-if="filteredEvents.length > 0" v-for="event in filteredEvents">
           <EventCard :event="event"  @click="navigateTo(`/events/${event.slug}`)"/>
         </template>
-        <p v-else class="italic font-semibold text-gray-600 dark:text-slate-300">No events found</p>.
+        <p v-else class="italic font-semibold text-gray-600 dark:text-slate-300">No events found in tag <span class="italic">{{ activeTag }}</span>.</p>.
       </div>
     </div>
   </div>
@@ -81,5 +81,15 @@ const filteredEvents = computed(() => {
 
 onUnmounted(() => {
   activeTag.value = '';
+})
+
+const tags = ref(useRoute().hash.replace('#', ''));
+watch(useRoute(), () => {
+  const hash = useRoute().hash;
+  if (hash) {
+    activeTag.value = hash.replace('#', '');
+  } else {
+    activeTag.value = '';
+  }
 })
 </script>
